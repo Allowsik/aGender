34c86971791cbbe0b632501103fc8eda64b49df8
Allowsik <bartekpingielski@gmail.com>
2022-03-24 15:14:08 +0100

    Code cleanup

--- src/main/java/pl/allowsik/Plec.java
+++ src/main/java/pl/allowsik/Plec.java
@@ -2,18 +2,17 @@ package pl.allowsik;
 
 import net.kyori.adventure.text.Component;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class Plec implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s, String[] args) {
         if (args.length == 1) {
-            if (args[0].toLowerCase().equals("female") || args[0].toLowerCase().equals("male")){
-                boolean isMale = false;
-                if (args[0].toLowerCase().equals("male")) isMale = true;
+            if (args[0].equalsIgnoreCase("female") || args[0].equalsIgnoreCase("male")){
+                boolean isMale = args[0].equalsIgnoreCase("male");
                 String gender = (isMale ? "&bMale" : "&dFemale");
                 sender.sendMessage(Component.text(ChatColor.translateAlternateColorCodes('&', "&x&f&f&0&0&f&fGENDER &cSuccessfully set your gender to: &f " + gender)));
             } else {
--- src/main/java/pl/allowsik/aGender.java
+++ src/main/java/pl/allowsik/aGender.java
@@ -4,12 +4,14 @@ package pl.allowsik;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class aGender extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getCommand("gender").setExecutor(new Plec());
+        Objects.requireNonNull(this.getCommand("gender")).setExecutor(new Plec());
     }
 
 }
