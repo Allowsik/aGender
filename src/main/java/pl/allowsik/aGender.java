17314de0ea49f997ccab618790048a8dfe181341
Allowsik <bartekpingielski@gmail.com>
2022-03-24 12:24:43 +0100

    Initial commit

---
+++ src/main/java/pl/allowsik/aGender.java
@@ -0,0 +1,17 @@
+package pl.allowsik;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class aGender extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
