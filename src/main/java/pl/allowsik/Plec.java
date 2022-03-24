dbe91c941c3008da7cb051f4d8d6721833cced85
Allowsik <bartekpingielski@gmail.com>
2022-03-24 13:15:22 +0100

    Added gender command

---
+++ src/main/java/pl/allowsik/Plec.java
@@ -0,0 +1,28 @@
+package pl.allowsik;
+
+import net.kyori.adventure.text.Component;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Plec implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+        Bukkit.getConsoleSender().sendMessage("Gruby jesteś XD");
+        if (args.length == 1) {
+            if (args[0].toLowerCase().equals("female") || args[0].toLowerCase().equals("male")){
+                boolean isMale = false;
+                if (args[0].toLowerCase().equals("male")) isMale = true;
+                String gender = (isMale ? "&bMale" : "&dFemale");
+                sender.sendMessage(Component.text(ChatColor.translateAlternateColorCodes('&', "&x&f&f&0&0&f&fGENDER &cSuccessfully set your gender to: &f " + gender)));
+            } else {
+                sender.sendMessage(Component.text(ChatColor.translateAlternateColorCodes('&', "&x&f&f&0&0&f&fGENDER &cCorrect usage: /gender <man/female>")));
+            }
+        } else {
+            sender.sendMessage(Component.text(ChatColor.translateAlternateColorCodes('&', "&x&f&f&0&0&f&fGENDER &cCorrect usage: /gender <man/female>")));
+        }
+        return false;
+    }
+}
--- src/main/java/pl/allowsik/aGender.java
+++ src/main/java/pl/allowsik/aGender.java
@@ -1,17 +1,15 @@
 package pl.allowsik;
 
+
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class aGender extends JavaPlugin {
+public final class aGender extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("gender").setExecutor(new Plec());
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
